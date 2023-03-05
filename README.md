9846c540d991ebf0ce5758238a5a8918f7fea58f
答案45 <gdp45@163.com>
2023-03-05 22:14:35 +0800

    生命周期提交

--- README.md
+++ README.md
@@ -22,3 +22,24 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### rem设置
+npm add postcss-loader postcss-pxtorem -D
+npm add autoprefixer@9.8.6 -D
+
+### less增加
+cnpm i less --save-dev
+cnpm install less-loader@7.3.0 --save-dev
+
+### 文件配置
+postcss.config.js
+vue.config.ts
+
+### 屏幕适配自适应d
+cnpm i lib-flexible --save
+npm install px2rem-loader --save-dev
+配置地址 https://www.jianshu.com/p/79be33f2ce88
+
+### vant UI组件库安装
+npm i vant -S
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <div id="tBox">{{ uname }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import { Vue, Options } from 'vue-class-component'
 @Options({
   components: {
-    HelloWorld,
-  },
+    // 组件声明
+  }
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue{
+  // 声明变量 不可使用关键字 var,let, const 并且必须有初始值
+  // 使用的时候使用this
+  uname:string = '小习'
+  // 生命周期里面进行方法执行
+  // 8个生命周期
+  // 数据的初始化一般是在 created/mounted 中操作
+  // 1. beforeCreate
+  public beforeCreate(): void {
+    console.log('beforeCreate', this.uname)
+  }
+  // 2. created
+  public created(): void {
+    console.log('created', this.uname)
+  }
+  // 3.beforeMount
+  public beforeMount (): void {
+    // 数据渲染前
+    console.log('beforeMount', this.uname)
+    console.log('beforeMount', document.querySelector('#tBox'))
+  }
+  // 4.mounted
+  public mounted (): void {
+    // 数据渲染后
+    console.log('mounted', this.uname)
+    console.log('mounted', document.querySelector('#tBox'))
+    setTimeout(() => {
+      this.uname = '小邓'
+    }, 2000)
+  }
+  // update数据更新并且引发了dom更新，就会触发
+  // 5.beforeUpdate
+  public beforeUpdate(): void {
+    console.log('beforeUpdate', this.uname)
+  }
+  // 6.update
+  public updated(): void {
+    console.log('updated', this.uname)
+  }
+  // destory 销毁
+  public beforeUnmount(): void {
+    // vue实例销毁前
+    console.log('beforeUnmount')
+  }
+  public unmounted(): void {
+    // vue实例销毁后
+    console.log('unmounted')
+  }
+}
 </script>
+<style lang="less" scoped>
+</style>
